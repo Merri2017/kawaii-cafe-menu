7912ff180aef1644e8f55147ed6c85eae97ee167
merri2017 <merrishel.richardson@gmail.com>
2018-08-19 12:56:33 -0400

    Added: more items to menu, prices and beverage images to Javascript, footer with contact info, materialize.css link in header.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
 // aim: display menu and images to the page 
 
-		var menu = ["Cucumber Sandwich", "Tonkatsu With Curry", "Tirimasu"];
-		var images= ['<img src="images/Cucumber.jpg">', '<img src="images/Tonkatsu.jpg">', '<img src="images/tiramisu.jpg">'];
+		var menu = ["Cucumber Sandwich", "Tonkatsu With Curry", "Tirimasu", "Assorted Beverages"];
+		var images= ['<img src="images/Cucumber.jpg" alt= "Cucumber Sandwich">', '<img src="images/Tonkatsu.jpg" alt= "Tonkatsu With Curry">', '<img src="images/tiramisu.jpg" alt = "Tiramisu Desert">', '<img src="images/beverages.jpg" alt = "Beverages">'];
+		var prices = ["$2.50", "$5.50", "$3.50", "$1.50"];
 		function displayMenu(){
 			for (var i = 0; i < menu.length; i++) {
-		 		document.getElementById('display').innerHTML += menu[i] += images[i] += "<br>";
+		 		document.getElementById('display').innerHTML += menu[i] += images[i] += prices[i] +="<br>";
 		 		// this allows the function to assign the images and display as the loop goes through each array. 
 					// document.getElementById('display').innerHTML += menu[i] += "<br>";
 					// += add the value of that expression to the value of the variable to the left of the operator,
